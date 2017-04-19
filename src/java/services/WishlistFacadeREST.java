4d0f28b9f6bd7a066b4703200b61164daf352c54
anhtuan161295 <QQ@DESKTOP-LJME648>
2017-04-19 20:45:31 +0700

    đang code dở wishlist

--- src/java/services/WishlistFacadeREST.java
+++ src/java/services/WishlistFacadeREST.java
@@ -5,11 +5,16 @@
  */
 package services;
 
+import entities.Customer;
+import entities.Product;
 import entities.Wishlist;
 import java.util.List;
+import java.util.UUID;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -18,14 +23,17 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author KID
  */
 @Stateless
-@Path("entities.wishlist")
+@Path("wishlist")
 public class WishlistFacadeREST extends AbstractFacade<Wishlist> {
+
     @PersistenceContext(unitName = "BookStoreProjectPU")
     private EntityManager em;
 
@@ -33,11 +41,24 @@ public class WishlistFacadeREST extends AbstractFacade<Wishlist> {
         super(Wishlist.class);
     }
 
+//    @POST
+//    @Override
+//    @Consumes({"application/xml", "application/json"})
+//    public void create(Wishlist entity) {
+//        super.create(entity);
+//    }
     @POST
-    @Override
+//    @Override
     @Consumes({"application/xml", "application/json"})
-    public void create(Wishlist entity) {
-        super.create(entity);
+    @Produces(MediaType.TEXT_PLAIN)
+    public boolean createWishlist(Wishlist entity) {
+        try {
+            entity.setId(UUID.randomUUID().toString());
+            super.create(entity);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @PUT
@@ -47,17 +68,41 @@ public class WishlistFacadeREST extends AbstractFacade<Wishlist> {
         super.edit(entity);
     }
 
+//    @DELETE
+//    @Path("{id}")
+//    public void remove(@PathParam("id") String id) {
+//        super.remove(super.find(id));
+//    }
     @DELETE
     @Path("{id}")
-    public void remove(@PathParam("id") String id) {
-        super.remove(super.find(id));
+    public boolean removeWishlist(@PathParam("id") String id) {
+        try {
+            super.remove(super.find(id));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
+//    @GET
+//    @Path("{id}")
+//    @Produces({"application/xml", "application/json"})
+//    public Wishlist find(@PathParam("id") String id) {
+//        return super.find(id);
+//    }
+
     @GET
-    @Path("{id}")
+    @Path("{customerId}/{productId}")
     @Produces({"application/xml", "application/json"})
-    public Wishlist find(@PathParam("id") String id) {
-        return super.find(id);
+    public Wishlist findByProductAndCustomer(@QueryParam("customerId") String customerId, @QueryParam("productId") String productId) {
+        Query q = em.createNamedQuery("Wishlist.findByCustomerAndProduct", Wishlist.class);
+        q.setParameter("customerId", customerId);
+        q.setParameter("productId", productId);
+        Wishlist w = null;
+        if (q.getResultList().size() > 0) {
+            w = (Wishlist) q.getResultList().get(0);
+        }
+        return w;
     }
 
     @GET
@@ -67,12 +112,12 @@ public class WishlistFacadeREST extends AbstractFacade<Wishlist> {
         return super.findAll();
     }
 
-    @GET
-    @Path("{from}/{to}")
-    @Produces({"application/xml", "application/json"})
-    public List<Wishlist> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        return super.findRange(new int[]{from, to});
-    }
+//    @GET
+//    @Path("{from}/{to}")
+//    @Produces({"application/xml", "application/json"})
+//    public List<Wishlist> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+//        return super.findRange(new int[]{from, to});
+//    }
 
     @GET
     @Path("count")
@@ -85,5 +130,5 @@ public class WishlistFacadeREST extends AbstractFacade<Wishlist> {
     protected EntityManager getEntityManager() {
         return em;
     }
-    
+
 }

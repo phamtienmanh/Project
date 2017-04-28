a3e0564f93bd5d2b594b84f9da13df3f2845881a
anhtuan161295 <QQ@10.49.21.3>
2017-04-28 16:28:53 +0700

    thêm change password

--- web/components/auth/user.service.js
+++ web/components/auth/user.service.js
@@ -3,16 +3,14 @@
 angular.module('shopnxApp')
   .factory('User', function ($resource) {
     return $resource('api/customer/changePassword', {
-      id: '@_id'
+//      id: '@_id'
     },
     {
       changePassword: {
         method: 'PUT',
         params: {
-//          id: '@_id',
-          password: '123' 
-//          oldPassword:'@_oldPassword',
-//          newPassword: '@_newPassword'
+          id: '@_id',
+          password: 'password' 
         }
       },
       get: {

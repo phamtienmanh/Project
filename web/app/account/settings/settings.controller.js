cb7c0039fc624b9680bed928aa451ffa21d93ef3
anhtuan161295 <QQ@10.48.237.3>
2017-05-03 15:39:06 +0700

    thêm change password

--- web/app/account/settings/settings.controller.js
+++ web/app/account/settings/settings.controller.js
@@ -3,19 +3,44 @@
 angular.module('shopnxApp')
   .controller('SettingsCtrl', function ($scope, User, Auth) {
     $scope.errors = {};
+    
+    $scope.checkPassword =function(password){
+        var currentUser = Auth.getCurrentUser();
+        if (password==currentUser.password) {
+            return true;
+        }
+        return false;
+    };
 
     $scope.changePassword = function(form) {
-      $scope.submitted = true;
-      if(form.$valid) {
-        Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
-        .then( function() {
-          $scope.message = 'Password successfully changed.';
-        })
-        .catch( function() {
-          form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
-          $scope.message = '';
+//      $scope.submitted = true;
+//      if(form.$valid) {
+//        Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
+//        .then( function() {
+//          $scope.message = 'Password successfully changed.';
+//        })
+//        .catch( function() {
+//          form.password.$setValidity('mongoose', false);
+//          $scope.errors.other = 'Incorrect password';
+//          $scope.message = '';
+//        });
+//      }
+        if(form.$valid){
+        if (!$scope.checkPassword($scope.user.oldPassword)) {
+            form.password.$setValidity('mongoose', false);
+            $scope.errors.other = 'Incorrect password';
+            $scope.message = '';
+        }else{
+            Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
+            .then( function() {
+              $scope.message = 'Password successfully changed.';
+            })
+            .catch( function() {
+              form.password.$setValidity('mongoose', false);
+              $scope.errors.other = 'Incorrect password';
+              $scope.message = '';
         });
-      }
+        }
+        }
 		};
   });

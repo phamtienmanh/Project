412f9e55577e2c3ad84ea36b99da22c18e6a566e
anhtuan161295 <QQ@DESKTOP-LJME648>
2017-05-06 21:54:21 +0700

    update biểu đồ

--- web/app/statistics/statistics.controller.js
+++ web/app/statistics/statistics.controller.js
@@ -4,6 +4,13 @@ angular.module('shopnxApp')
   .controller('StatisticsCtrl', function ($rootScope, $scope, Auth ,Order) { //, socket, Category, Modal, toastr
 //  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   // Lấy tất cả các tháng
+  $scope.datepickerOptions = {
+    datepickerMode:"'year'",
+    minMode:"'year'",
+    minDate:"minDate",
+    showWeeks:"false"
+ };
+      
   $scope.labels = moment.months();
   $scope.series = ['Total'];
 
@@ -12,10 +19,32 @@ angular.module('shopnxApp')
   
 //    $scope.dt1 = moment().startOf('month').subtract(7, "days")._d;
   $scope.dt1 = moment().startOf('year')._d;
-  $scope.dt2 = moment().endOf('year')._d;
+  $scope.dt2 = moment($scope.dt1).endOf('year')._d;
   $scope.today = moment().endOf('day')._d;
   $scope.orders = {};
+  
+  $scope.getDayClass = function (date, mode) {
+                    if (mode === 'day') {
+                        var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+                        var currentDay1 = new Date($scope.dt1).setHours(0, 0, 0, 0);
+                        var currentDay2 = new Date($scope.dt2).setHours(0, 0, 0, 0);
+                        if (dayToCheck === currentDay1 && currentDay1 === currentDay2) {
+                            return 'samedate';
+                        }
+                        if (dayToCheck === currentDay1) {
+                            return 'datetime1';
+                        }
+                        if (dayToCheck === currentDay2) {
+                            return 'datetime2';
+                        }
+                    }
+                    return '';
+                };
+  
   $scope.loadOrders = function () {
+    $scope.data = [[0, 0, 0, 0, 0, 0, 0,0,0,0,0,0]];
+    $scope.dt2 = moment($scope.dt1).endOf('year')._d;
+
                     if ($rootScope.isAdmin() == true) {
                         $scope.orders = Order.alldelivered.query({from: moment($scope.dt1).startOf('day').unix(), to: moment($scope.dt2).endOf('day').unix()}, function (res) {
                             var total = 0;
@@ -41,5 +70,11 @@ angular.module('shopnxApp')
         $scope.loadOrders();
 //        $scope.labels = $scope.orders.date.year;
                 
-  
+    $scope.popup1 = {
+            opened: false
+                };
+    $scope.popup2 = {
+            opened: false
+                };
+    
   });
